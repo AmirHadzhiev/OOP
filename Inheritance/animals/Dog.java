e6de610e89a20c61014263b17325eeedbaeb4d8a
AmirHadzhiev <104004036+AmirHadzhiev@users.noreply.github.com>
2022-08-24 00:45:07 -1100

    Add files via upload

---
+++ Inheritance/animals/Dog.java
@@ -0,0 +1,12 @@
+package com.softuni.Inheritance.animals;
+
+public class Dog extends Animal{
+    public Dog(String name, int age, String gender) {
+        super(name, age, gender);
+    }
+
+    @Override
+    public String produceSound(){
+        return "Woof!";
+    }
+}
