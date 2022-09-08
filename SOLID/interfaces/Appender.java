a349c75861ad208ba023e9aa9598a4cb61bae829
AmirHadzhiev <104004036+AmirHadzhiev@users.noreply.github.com>
2022-09-07 19:05:07 -1100

    Add files via upload

---
+++ SOLID/interfaces/Appender.java
@@ -0,0 +1,8 @@
+package com.softuni.SOLID.interfaces;
+
+import com.softuni.SOLID.enums.ReportLevel;
+
+public interface Appender {
+    void append(String time, String message, ReportLevel reportLevel);
+    void setReportLevel(ReportLevel reportLevel);
+}
